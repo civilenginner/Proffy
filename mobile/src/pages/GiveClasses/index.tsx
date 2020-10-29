d2a39a376ef56bde93d04761853ab8c29cae11f8
wagner maidson <wagner.rmaidson@gmail.com>
2020-10-29 14:39:54 -0300

    :fire: create mobile application

---
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View, ImageBackground, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+
+import styles from './styles';
+
+import giveCLassesBgImage from '../../assets/images/give-classes-background.png';
+
+function GiveClasses() {
+  const { goBack } = useNavigation();
+
+  function handleNavigateBack() {
+    goBack();
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        resizeMode="contain"
+        source={giveCLassesBgImage}
+        style={styles.content}
+      >
+        <Text style={styles.title}>
+            Want to be a Proffy?
+        </Text>
+        <Text style={styles.description}>
+            To start, you need to register as a teacher on our web platform.
+        </Text>
+      </ImageBackground>
+      <RectButton 
+        style={styles.okButton}
+        onPress={handleNavigateBack}
+      >
+        <Text style={styles.okButtonText}>it's all right</Text>
+      </RectButton>
+    </View>
+  );
+}
+
+export default GiveClasses;
---
+++ mobile/src/pages/Landing/index.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react'
+import { View, Text, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import api from '../../services/api';
+import styles from './styles';
+
+import landingImg from '../../assets/images/landing.png';
+import studyIcon from '../../assets/images/icons/study.png';
+import giveClassIcon from '../../assets/images/icons/give-classes.png';
+import heartIcon from '../../assets/images/icons/heart.png';
+
+function Landing() {
+  const { navigate } = useNavigation();
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    });
+  }, []);
+
+  function handleNavigateToGiveClassesPage() {
+    navigate('GiveClasses');
+  }
+
+  function handleNavigateToStudyPages() {
+    navigate('Study');
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image source={landingImg} style={styles.banner} />
+
+      <Text style={styles.title}>
+        Be welcome, {'\n'}
+        <Text style={styles.titleBold}>What do you wanna do?</Text>
+      </Text>
+
+      <View style={styles.buttonsContainer}>
+        <RectButton
+          onPress={handleNavigateToStudyPages}
+          style={[styles.button, styles.buttonPrimary]}
+        >
+          <Image source={studyIcon} />
+          <Text style={styles.buttonText}>To study</Text>
+        </RectButton>
+
+        <RectButton
+          onPress={handleNavigateToGiveClassesPage}
+          style={[styles.button, styles.buttonSecondary]}
+        >
+          <Image source={giveClassIcon} />
+          <Text style={styles.buttonText}>Give classes</Text>
+        </RectButton>
+      </View>
+
+      <Text style={styles.totalConnections}>
+        Total of {totalConnections} connections made already {''}
+        <Image source={heartIcon} />
+      </Text>
+    </View>
+  )
+}
+
+export default Landing;
